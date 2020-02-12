4ae317886244241b6348cad5806138dea5071960
Anthony <awrodriguezt@gmail.com>
2020-02-11 19:45:28 -0600

    Correcciones

--- routes/api/fotografias/fotografias.model.js
+++ routes/api/fotografias/fotografias.model.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var fileToSave = 'security.json';
+var fileToSave = 'fotografias.json';
 
 var fotografiasModel = {};
 
@@ -35,19 +35,6 @@ openFile();
 fotografiasModel.getAll = ()=>{
     return fotografiasCollection
 }
-
-fotografiasCollection.push(
-    Object.assign(
-        {},
-        fotografiasTemplate, 
-        {
-            id:1,
-            title:"Snow Mountain",
-            url:"https://unsplash.com/photos/zlPhxd5OydQ",
-            thumbnailURL:"https://images.unsplash.com/photo-1531436107035-40b2e85b7a1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-            album:"Mountains"
-        })
-);
 
 fotografiasModel.addNew=({sent_title, sent_url, sent_thumbnailURL, sent_album} )=>{
     var newfoto = Object.assign(
@@ -107,5 +94,21 @@ fotografiasModel.deleteByID = (ID)=>{
     writeToFile();    
     return true;
 }
+
+/*
+fotografiasCollection.push( 
+    Object.assign(
+        {},
+        fotografiasTemplate, 
+        {
+            id:1,
+            title:"Snow Mountain",
+            url:"https://unsplash.com/photos/zlPhxd5OydQ",
+            thumbnailURL:"https://images.unsplash.com/photo-1531436107035-40b2e85b7a1b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+            album:"Mountains"
+        })
+);
+*/
+
 
 module.exports = fotografiasModel;
